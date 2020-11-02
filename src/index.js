fba56c06716e16a3f605963f3194a5f09f39fd07
Lauren Dove <elledoveinc@gmail.com>
2020-11-02 09:32:51 -0500

    created folders for action, components, reducers. also set up devtools with compose function.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware,compose} from 'redux';
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
 
 
-const store = createStore();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = compose(reducer,composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
