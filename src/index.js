a23d3ca39b4a05c03527f037fa28a65846b9b573
Lauren Dove <elledoveinc@gmail.com>
2020-11-02 10:13:17 -0500

    menureducer has been created and imported into index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore, applyMiddleware,compose} from 'redux';
-import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import menuReducer from './reducers/menuReducer';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = compose(reducer,composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(menuReducer,composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducers/menuReducer.js
@@ -0,0 +1,4 @@
+export default function menuReducer(state = {menu_items:[]},action){
+    return state
+
+}
